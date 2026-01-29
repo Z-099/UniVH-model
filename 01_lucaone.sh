339cb543c671a63254b941cc47b926a579617c10
Z-099 <dzhao089@gmail.com>
2026-01-29 11:18:26 +0800

    Update 01_lucaone.sh

--- 01_lucaone.sh
+++ 01_lucaone.sh
@@ -1,8 +1,25 @@
-# awk '/^>/ {sub(/ .*/, "", $0)} {print}' ../data/test.fasta > ../data/test_clean.fasta
+#!/usr/bin/env bash
+set -euo pipefail
 
-cd ../LucaOneApp/LucaOneApp-master/algorithms
+# Default values (can be overridden by CLI args)
+INPUT_FILE="./UniVH-model-main/test/PV241319.1_nt.fasta"
+SAVE_PATH="./UniVH-model-main/test/embedding"
+GPU_ID="0"
+
+# Parse args
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    --input_file) INPUT_FILE="$2"; shift 2 ;;
+    --save_path)  SAVE_PATH="$2"; shift 2 ;;
+    --gpu_id)     GPU_ID="$2"; shift 2 ;;
+    *) echo "Unknown argument: $1" >&2; exit 1 ;;
+  esac
+done
+
+cd ./UniVH-model-main/LucaOneApp/LucaOneApp-master/algorithms
+
 python inference_embedding_lucaone.py \
-    --llm_dir /data3/zd/LucaOneApp/models \
+    --llm_dir ./UniVH-model-main/LucaOneApp/models \
     --llm_type lucaone_gplm \
     --llm_version v2.0 \
     --llm_task_level token_level,span_level,seq_level,structure_level \
@@ -11,11 +28,11 @@ python inference_embedding_lucaone.py \
     --truncation_seq_length 100000 \
     --trunc_type right \
     --seq_type gene \
-    --input_file ../test/PV241319.1_nt.fasta \
-    --save_path ../test/embedding \
+    --input_file "${INPUT_FILE}" \
+    --save_path "${SAVE_PATH}" \
     --embedding_type vector \
     --matrix_add_special_token \
     --embedding_complete \
     --embedding_complete_seg_overlap \
     --embedding_fixed_len_a_time 1000 \
-    --gpu_id 0
+    --gpu_id "${GPU_ID}"
